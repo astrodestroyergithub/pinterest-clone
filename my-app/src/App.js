0d90dc768488810430fb964ba9d193ba2e1576ee
Tamojit Roy <astrodestroyer146@gmail.com>
2022-11-14 15:07:41 +0530

    now its ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,17 +4,22 @@ import Employee from './components/Employee';
 function App() {
   console.log('We are about to list the employees');
   const showEmployees = true;
-  return (showEmployees ? (
-      <>
-        <Employee />
-        <Employee />
-        <Employee />
-        <Employee />
-        <Employee />
-      </>
-    ) :
-      <p>We can not show you the employees</p>
-  );
+    return (
+            <div className="App">
+              { console.log('We are inside the return') }
+              { showEmployees ? (
+                <>
+                  <Employee />
+                  <Employee />
+                  <Employee />
+                  <Employee />
+                  <Employee />
+                </>
+              ) :
+                <p>We can not show you the employees</p>
+              }
+            </div>
+    );
 }
 
 export default App;
